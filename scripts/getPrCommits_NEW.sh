25a05be79d232067f0e319211b86e8da4bb937a4
LetMeFly666 <814114971@qq.com>
2025-01-27 10:41:45 +0800

    feat: get new commits on 1 pr
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- scripts/getPrCommits_NEW.sh
+++ scripts/getPrCommits_NEW.sh
@@ -1,24 +1,25 @@
 ###
  # @Author: LetMeFly
- # @Date: 2025-01-27 09:32:48
+ # @Date: 2025-01-27 10:36:25
  # @LastEditors: LetMeFly.xyz
- # @LastEditTime: 2025-01-27 10:39:35
+ # @LastEditTime: 2025-01-27 10:40:09
 ### 
 ###
  # 获取某PR的所有commit Hash
  # 前提: 环境变量$GITHUB_TOKEN
  # Input: 
- #   - PR编号$PR_NUMBER
+ #   - 触发这次流程前的commit_sha $BEFORE_SHA
+ #   - 触发这次流程时的commit_sha $AFTER_SHA
  #   - 仓库名$REPO_FULL_NAME（例如LetMeFly666/secret-monitor）
- # Output: /tmp/pr_commits_all.txt（每行一个commit Hash）
+ # Output: /tmp/pr_commits_new.txt（每行一个commit Hash）
 ###
 
 # ----------------------------- 检查必要环境变量 -----------------------------
 if [[ "$(uname -o)" == "Msys" ]]; then  # Windows
-    REQUIRED_VARS=("PR_NUMBER" "REPO_FULL_NAME")
+    REQUIRED_VARS=("BEFORE_SHA" "AFTER_SHA" "REPO_FULL_NAME")
     export MSYS_NO_PATHCONV=1  # 否则的话，Git bash在Windows上会将/api转为F:/xxx/api
 else
-    REQUIRED_VARS=("GITHUB_TOKEN" "PR_NUMBER" "REPO_FULL_NAME")
+    REQUIRED_VARS=("GITHUB_TOKEN" "BEFORE_SHA" "AFTER_SHA" "REPO_FULL_NAME")
 fi
 missing_vars=0
 for var in ${REQUIRED_VARS[@]}; do
@@ -34,8 +35,7 @@ echo "✅ 所有必要环境变量已设置"
 
 
 # ----------------------------- 获取commit记录 -----------------------------
-# https://docs.github.com/zh/rest/pulls/pulls?apiVersion=2022-11-28#list-commits-on-a-pull-request
-GH_APIURI="/repos/$REPO_FULL_NAME/pulls/$PR_NUMBER/commits"
+GH_APIURI="/repos/$REPO_FULL_NAME/compare/$BEFORE_SHA...$AFTER_SHA"
 echo "GH_APIURI: $GH_APIURI"
 RESPONSE=$(gh api \
     -H "Accept: application/vnd.github+json" \
@@ -47,7 +47,7 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # ----------------------------- 将结果写入文件 -----------------------------
-echo "$RESPONSE" | jq -r '.[].sha' > /tmp/pr_commits_all.txt
+echo "$RESPONSE" | jq -r '.[].sha' > /tmp/pr_commits_new.txt
 echo "***BEGIN CAT***"
-cat /tmp/pr_commits_all.txt
+cat /tmp/pr_commits_new.txt
 echo "***END CAT***"
